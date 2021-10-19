d61b9d7192689e8d2200c7c732a41f54f106a8b7
Scott Jaworski <scottpjaworski@gmail.com>
2021-10-19 16:30:35 -0400

    disable function still not working, hover on h2 working as intended

--- app.js
+++ app.js
@@ -8,22 +8,37 @@ if ( $("#btnSubmit").val() !== '') {
    $('#btnSubmit').prop('disabled', false);
 };
 
+$('#btnSubmit').click(function() {
+   alert($("input[id=textSubmit]").val());
+});
+
 let containerDiv = document.createElement('div');
 document.body.append(containerDiv);
+
+$(document).on('mouseover', '.headerList', function() {
+   console.log('hovering');
+   $(this).css({
+      'background-color': 'red',
+      'border-radius': '10px'
+   })
+})
+
+$(document).on('mouseleave', '.headerList', function(){
+   console.log('mouse has left');
+   $(this).css({
+      'background-color': 'white',
+      'border-radius': '0px'
+   })
+})
 
 $('#btnSubmit').click(function() {
    let inputItem = document.createTextNode(( $("input[id=textSubmit]").val())); 
    let newHeader = document.createElement('h2');
    newHeader.className = 'headerList';
-   newHeader.appendChild(inputItem);
-   containerDiv.appendChild(newHeader);
+   newHeader.append(inputItem);
+   containerDiv.append(newHeader);
    return false;
 });
-$('.headerList').hover(function() {
-   $('.headerList').css({
-      'background-color': 'red',
-      'border-radius': '10px'
-   })
-}
-)
+
+
 });
