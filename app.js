c460cfcc12dcf8e94677f586a165e3ab253920a2
Scott Jaworski <scottpjaworski@gmail.com>
2021-10-19 14:20:15 -0400

    jQuery not activating mouseover events, diasable not working

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ $(document).ready(function(){
 
 let submitBtn = $("#btnSubmit");
 
-$('#btnSubmit').click(function() {
-   alert( $("input[id=textSubmit]").val()); 
-});
+$('#btnSubmit').prop('disabled', true);
 
+if ( $("#btnSubmit").val() !== '') {
+   $('#btnSubmit').prop('disabled', false);
+};
+
+let containerDiv = document.createElement('div');
+document.body.append(containerDiv);
+
+$('#btnSubmit').click(function() {
+   let inputItem = document.createTextNode(( $("input[id=textSubmit]").val())); 
+   let newHeader = document.createElement('h2');
+   newHeader.className = 'headerList';
+   newHeader.appendChild(inputItem);
+   containerDiv.appendChild(newHeader);
+   return false;
+});
+$('.headerList').hover(function() {
+   $('.headerList').css({
+      'background-color': 'red',
+      'border-radius': '10px'
+   })
+}
+)
 });
